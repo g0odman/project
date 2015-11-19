9a3ae071f6e48962c2f5b4464385401ef3f06c8f
Alexander Ar-Staelin <alexander@datainsamling-ftp.lansstyrelsen.se>
2015-11-19 08:31:01 +0200

    added function descriptions, as well as comments.

--- SP_Aux.c
+++ SP_Aux.c
@@ -3,9 +3,16 @@
 #include <stdlib.h>
 #include "SP_Aux.h"
 
-
+/**
+ *  Main parsing function.
+ *  
+ *  Input is a line read from user
+ */
 double parse(char * line){
+	
     char * tok = strtok(line,"\t\r\n");
+    
+    //TODO finish:
     while(tok != NULL){
         if(get_type(tok) == NUMBER)
             printf("YAY\n");
@@ -16,12 +23,20 @@ double parse(char * line){
     return 0;
 }
 
+/**
+ *  Returns type of given token read from user
+ *  
+ */
 SP_STACK_ELEMENT_TYPE get_type(char * tok){
+	
+	//why not already convert it to an SP_STACK_ELEMENT, and return that?
     if(is_number(tok))
         return NUMBER;
     return PLUS;
 }
 
+
+//isn't there a buit in function that does this?
 bool is_number(char * tok){
     int i =0;
     while(tok[i] != '\0'){
@@ -31,10 +46,20 @@ bool is_number(char * tok){
     }
     return true;
 }
+
+/**
+ *  Returns true if the given input is the exit command, false otherwise
+ *  
+ */
 bool is_exit(char * tok){
     char * temp = (char *) malloc(3);
     strncpy(temp,tok,2);
-    if(strcmp(temp,"<>")==0 && strlen(tok) == 3)
+    
+    if(strcmp(tok,"<>") == 0 && strlen(tok) == 3){
+    	free(temp); //you need to free it!
         return true;
+    }
+    
+    free(temp); //you need to free it!
     return false;
 }
