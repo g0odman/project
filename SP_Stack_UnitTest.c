a330b441fde186eedefa046bf19ff098754ff171
Alexander Ar-Staelin <alexander@Alexanders-MacBook-Pro.local>
2015-11-22 22:09:48 +0200

    added some stack testing

--- SP_Stack_UnitTest.c
+++ SP_Stack_UnitTest.c
@@ -34,7 +34,7 @@ bool TEST1(){
 	elementPtr = spStackTop(stack,&msg);
 	ASSERT_TRUE(msg == SP_STACK_ERROR_IS_EMPTY && elementPtr == NULL);
 	SP_STACK_ELEMENT newElement = {.type = NUMBER , .value = 100};
-	stack = spStackPush(stack,newElement,&msg); //memory leak?
+	stack = spStackPush(stack,newElement,&msg);
 	ASSERT_TRUE(msg == SP_STACK_SUCCESS);
 	elementPtr = spStackTop(stack,&msg);
 	ASSERT_TRUE(elementPtr->type == newElement.type && elementPtr->value == newElement.value && msg==SP_STACK_SUCCESS);
@@ -42,7 +42,35 @@ bool TEST1(){
 	return 1;
 }
 
+bool TEST2(){
+	
+	SP_STACK_MSG msg;
+	SP_STACK_ELEMENT* top = NULL;
+
+	
+	//make empty stack
+	SP_STACK* stack = spStackCreate(&msg);
+	ASSERT_TRUE(spStackIsEmpty(stack, &msg));
+	
+	//insert numbers 0 to 99:
+	for(int i=0; i<100; i++){
+		SP_STACK_ELEMENT new = {.type = NUMBER, .value = i};
+		stack = spStackPush(stack,new,&msg);
+		ASSERT_TRUE(msg == SP_STACK_SUCCESS);
+		
+		top = spStackTop(stack, &msg);
+		ASSERT_TRUE(top->type == new.type && top->value == new.value && msg==SP_STACK_SUCCESS);
+	}
+	
+	ASSERT_TRUE(spStackIsEmpty(stack, &msg) == false);
+	
+	spStackDestroy(stack);
+	
+	return 1;
+}
+
 int main(){
 	RUN_TEST(TEST1);
+	RUN_TEST(TEST2);
 	return 0;
 }
